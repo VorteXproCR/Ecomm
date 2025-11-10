f042427463370355427db71379734eb02f06c343
Anish Sayana <anishsayana@gmail.com>
2025-11-10 14:06:10 +0530

    Frontend

---
+++ Client/src/components/Prices.js
@@ -0,0 +1,22 @@
+export const Prices = [
+    {
+        _id:0,
+        name:" ʛ 0 to  ʛ 250",
+        array:[0,250]
+    },
+    {
+        _id:1,
+        name:' ʛ 250 to ʛ 500',
+        array:[250,500]
+    },
+    {
+        _id:2,
+        name:' ʛ 500 to ʛ 1000',
+        array:[500,1000]
+    },
+    {
+        _id:3,
+        name:' ʛ 1000 or ʛ more',
+        array:[1000,99999]
+    },
+];
